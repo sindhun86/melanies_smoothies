1b3d6cf6782bd0f4cac315397877c36a0aebc1f3
sindhun86 <sindhun86@gmail.com>
2025-06-23 04:11:04 -0400

    Update streamlit_app.py
    test17

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,14 @@ st.write("The current movie title is", title)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections = 5
 )
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
